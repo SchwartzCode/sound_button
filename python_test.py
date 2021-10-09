79ee2bb99b7344d8a57971d2be15b908204405c9
SchwartzCode <jonbs98@gmail.com>
2021-10-08 20:44:59 -0400

    button is working!

--- python_test.py
+++ python_test.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import RPi.GPIO as GPIO
+import time
+
+button_pin = 3
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(button_pin, GPIO.IN)
+
+while True:
+    input = GPIO.input(button_pin)
+    print(input)
+    time.sleep(1)
+
+
