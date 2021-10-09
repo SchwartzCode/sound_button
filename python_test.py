1e8a4193e95c4b2f24d69b4a3a8232d534e0373e
SchwartzCode <jonbs98@gmail.com>
2021-10-09 00:01:51 -0400

    made class to play sounds

--- python_test.py
+++ python_test.py
@@ -1,17 +1,36 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 
-button_pin = 3
+class SoundPlayer(object):
 
+    def __init__(self, sounds):
+        print("init")
+        self.sounds = sounds
+
+    def playSound(self):
+        print("should be playing a sound")
+
+# board pinout:
+# https://iot4beginners.com/difference-between-bcm-and-board-pin-numbering-in-raspberry-pi/
 GPIO.setmode(GPIO.BOARD)
 
+button_pin = 3
 GPIO.setup(button_pin, GPIO.IN)
+
+sounds = []
+
+noisey_guy = SoundPlayer(sounds)
+
 
 while True:
     input = GPIO.wait_for_edge(button_pin, GPIO.RISING)
 
     if input:
         print("pressed!")
+        noisey_guy.playSound()
+
+    time.sleep(0.1)
+
 
 # while True:
 #     input = GPIO.input(button_pin)
